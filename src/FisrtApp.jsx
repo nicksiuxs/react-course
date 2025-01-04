60fecd5f471eba4885b1c4d832428f9900c2ad51
Nicolas Londono <nicksiuxs@gmail.com>
2025-01-03 20:38:07 -0500

    Add: some test

--- src/FisrtApp.jsx
+++ src/FisrtApp.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 function FirstApp({ title, subtitle, name }) {
   return (
     <>
-      <h1>{title}</h1>
+      <h1 data-testid="title">{title}</h1>
       <p>{subtitle}</p>
       <p>{name}</p>
     </>
--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -2,23 +2,39 @@ import { render } from "@testing-library/react";
 import FirstApp from "../src/FisrtApp";
 
 describe("Test in FirstApp.test.jsx", () => {
-  test("Should match the component with the snapshot", () => {
-    const title = "Soy un titulo";
+  // The snapshot test is better to do when a component is finished and it will not change
+  // test("Should match the component with the snapshot", () => {
+  //   const title = "Soy un titulo";
 
-    const { container } = render(<FirstApp title={title} />);
+  //   const { container } = render(<FirstApp title={title} />);
 
-    expect(container).toMatchSnapshot();
-  });
+  //   expect(container).toMatchSnapshot();
+  // });
 
   test("Should show the title sent by props in a h1", () => {
     const title = "Soy un titulo";
 
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const { container, getByText, getByTestId } = render(
+      <FirstApp title={title} />
+    );
 
     expect(getByText(title)).toBeTruthy();
 
     // This is not the best way to test this
-    const h1 = container.querySelector("h1");
-    expect(h1.innerHTML).toContain(title);
+    // const h1 = container.querySelector("h1");
+    // expect(h1.innerHTML).toContain(title);
+
+    expect(getByTestId("title").textContent).toBe(title);
+  });
+
+  test("Should show the subtitle sent by props ", () => {
+    const title = "Soy un titulo";
+    const subtitle = "Soy un subtitulo";
+
+    const { getByText } = render(
+      <FirstApp title={title} subtitle={subtitle} />
+    );
+
+    expect(getByText(subtitle)).toBeTruthy();
   });
 });
